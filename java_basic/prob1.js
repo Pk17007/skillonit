a28475dcf048ede128b0c961cdfa809250a2abd2
Pk17007 <niparthkhillare@gmail.com>
2025-06-18 17:36:06 +0530

    Committ

---
+++ java_basic/prob1.js
@@ -0,0 +1,26 @@
+let a = "This is a 123";
+let b = 14563;
+let c = false;
+
+// console.log (typeof a)
+// console.log (a)
+
+// console.log (typeof b)
+// console.log (b)
+
+// console.log (typeof c)
+// console.log (l
+
+
+                            // Operators//
+let x = 3;
+let y = 4;
+console.log ("Operators.exe")
+console.log ("Addition", x + y)
+console.log ("Subtraction", x - y)
+console.log ("Multiply", x * y)
+console.log ("divide", x / y)
+console.log ("Exponent", x ** y)
+console.log ( "Reminder",x%y)
+
+
