3a41ba09616e3e276c328964eed8f16a26ec3d72
Pk17007 <niparthkhillare@gmail.com>
2025-06-18 21:56:13 +0530

    c-8

---
+++ java_basic/lvc.js
@@ -0,0 +1,29 @@
+// let x; 
+// console.log (x)
+// Undefined
+
+// let x=1;
+// console.log (x)
+// 1
+
+// console.log (x)
+// let x=1
+// ReferenceError: Cannot access 'x' before initialization
+
+// var x;
+// console.log(x)
+// undefined  not declared
+
+// var x;
+// x= 12;
+// console.log (x)
+// 12
+
+// x=32;
+// var x;
+// console.log (x)
+// 32          surprise
+
+
+
+
--- java_basic/operator.js
+++ java_basic/operator.js
@@ -1,6 +1,6 @@
-let a = "This is a 123";
-let b = 14563;
-let c = false;
+// let a = "This is a 123";
+// let b = 14563;
+// let c = false;
 
 // console.log (typeof a)
 // console.log (a)
@@ -9,34 +9,34 @@ let c = false;
 // console.log (b)
 
 // console.log (typeof c)
-// console.log (l
+// console.log (c)
 
 
 //                             // Operators//
-// let x = 3;
-// let y = 4;
-// console.log ("Operators.exe")
-// console.log ("Addition", x + y)
-// console.log ("Subtraction", x - y)
-// console.log ("Multiply", x * y)
-// console.log ("divide", x / y)
-// console.log ("Exponent", x ** y)
-// console.log ( "Reminder",x%y)
+let x = 3;
+let y = 4;
+console.log ("Operators.exe")
+console.log ("Addition", x + y)
+console.log ("Subtraction", x - y)
+console.log ("Multiply", x * y)
+console.log ("divide", x / y)
+console.log ("Exponent", x ** y)
+console.log ( "Reminder",x%y)
 
 
 
 // console.log (a+b+c);
 // everything is well and good
-//now changing the datatype
+// //now changing the datatype
 
-let x = "165"
-let y = 34
-let z = "abcd"
+// let x = "165"
+// let y = 34
+// let z = "abcd"
 
-console.log (x+y)
-console.log (y+x)
-console.log (x+z)
-console.log (z+x)
+// console.log (x+y)
+// console.log (y+x)
+// console.log (x+z)
+// console.log (z+x)
 
 
 
