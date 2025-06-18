d7ccc28a70f958b5ef4eccebe6c90a0527fd2f2c
Pk17007 <niparthkhillare@gmail.com>
2025-06-18 17:45:33 +0530

    c6

--- java_basic/prob2.js
+++ java_basic/prob2.js
@@ -5,6 +5,19 @@ let a = 10
 let b = 20 
 let c = 90
 
-console.log (a>=b);
+// console.log (a>=b);
+// console.log (a+b+c);
+// everything is well and good
+//now changing the datatype
+
+let x = "165"
+let y = 34
+let z = "abcd"
+
+console.log (x+y)
+console.log (y+x)
+console.log (x+z)
+console.log (z+x)
+
 
 
