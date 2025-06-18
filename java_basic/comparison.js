c7718165d2da1b0ca1ad1c724d4a232570cf8b66
Pk17007 <niparthkhillare@gmail.com>
2025-06-18 17:49:33 +0530

    c6

---
+++ java_basic/comparison.js
@@ -0,0 +1,19 @@
+// comperator
+
+
+let a = 10
+let b = "10" 
+let c = 90
+
+console.log (a>=b);
+console.log (a>=c);
+console.log (a>b);
+console.log (a==b);
+console.log (a===b);
+
+// ANSWERS
+// true
+// false
+// false
+// true
+// false
