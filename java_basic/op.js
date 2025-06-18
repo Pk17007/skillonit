d898453cf93b71c962670c33c436a8688a77086d
Pk17007 <niparthkhillare@gmail.com>
2025-06-18 17:47:10 +0530

    c6

---
+++ java_basic/op.js
@@ -0,0 +1,10 @@
+// comperator
+
+
+let a = 10
+let b = 20 
+let c = 90
+
+// console.log (a>=b);
+
+
--- java_basic/operator.js
+++ java_basic/operator.js
@@ -12,15 +12,31 @@ let c = false;
 // console.log (l
 
 
-                            // Operators//
-let x = 3;
-let y = 4;
-console.log ("Operators.exe")
-console.log ("Addition", x + y)
-console.log ("Subtraction", x - y)
-console.log ("Multiply", x * y)
-console.log ("divide", x / y)
-console.log ("Exponent", x ** y)
-console.log ( "Reminder",x%y)
+//                             // Operators//
+// let x = 3;
+// let y = 4;
+// console.log ("Operators.exe")
+// console.log ("Addition", x + y)
+// console.log ("Subtraction", x - y)
+// console.log ("Multiply", x * y)
+// console.log ("divide", x / y)
+// console.log ("Exponent", x ** y)
+// console.log ( "Reminder",x%y)
+
+
+
+// console.log (a+b+c);
+// everything is well and good
+//now changing the datatype
+
+let x = "165"
+let y = 34
+let z = "abcd"
+
+console.log (x+y)
+console.log (y+x)
+console.log (x+z)
+console.log (z+x)
+
 
 
