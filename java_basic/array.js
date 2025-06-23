660863b3f4eb4b416c6fbd76856ca3605df0fa17
Pk17007 <niparthkhillare@gmail.com>
2025-06-23 14:24:19 +0530

    c-9

--- java_basic/array.js
+++ java_basic/array.js
@@ -1,5 +1,6 @@
-var m1= ["n1","n2",23, 24, 25];
+var m1= ["n1","n2", "manchester",23, 24, 25];
         console.log(m1 [5]);
         console.log (m1.length);
-m1.push("werq");
-m1.pop("23");
+m1.unshift("werq");
+
+ console.log (m1)
