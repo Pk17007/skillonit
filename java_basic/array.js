7dd871f9f97f6b46f4179c493603d22bd20eb443
Pk17007 <niparthkhillare@gmail.com>
2025-06-23 15:43:36 +0530

    c-9

--- java_basic/array.js
+++ java_basic/array.js
@@ -1,6 +1,25 @@
-var m1= ["n1","n2", "manchester",23, 24, 25];
-        console.log(m1 [5]);
-        console.log (m1.length);
-m1.unshift("werq");
+var m1= ["n1","n2", "manchester",23, 24, 5, [1,2,3,4,5]];
+        // console.log(m1 [5]);
+        // console.log (m1.length);
+        
+// m1.unshift("werq");
+        // let search = m1 [7][3];                         REally imp
+        //  output is 4;;;
+        //  console.log (search)
 
- console.log (m1)
+// m1.push(123)                    //ADD item at last
+// console.log (m1)
+// m1.pop("")                      // Deletes last item
+// console.log (m1)
+
+// console.log(m1.length)                 // tells length of array 
+
+// console.log (m1.indexOf (5))           // tells index of the item in paranthesis which appears first 
+
+// m1.unshift("london")                    // add item at start
+// m1.shift()                              // remove items at start 
+// console.log (m1)
+
+
+
+
