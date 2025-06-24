cb8df1bc18ee508d6d83b7b88f5ecd85cd740e31
Pk17007 <niparthkhillare@gmail.com>
2025-06-24 13:46:09 +0530

    c-10

---
+++ java_basic/ans1.js
@@ -0,0 +1,207 @@
+// 2. Write a program to check if a number is even and greater than 10.
+
+// let a = 5;
+
+// if(a>10){
+//     if(a%2==0){
+//         console.log(a,"is even no and is greater than 10");
+//     }else{
+//         console.log(a,"is odd no and is greater than 10");
+//     }
+// }else if(a%2==0){
+//         console.log(a,"is even no and is less than 10");
+//     }else{
+//         console.log(a,"is odd no and is less than 10");
+//     }
+
+/////////////////////////////////////////////////////////////////////////
+// 4]Use a ternary operator to check if a number is positive or negative.
+// // 
+// i = 0;
+// if(i<0){
+//     console.log(i,"is a negative number");
+// }else if (i>0) {
+//     console.log(i,"is a positive number");
+// }else{
+//     console.log(i,"is a neither a postive or negative number!!");
+// }
+
+/////////////////////////////////////////////////////////////////////////
+///5. Write a program to calculate the factorial of a number using a `while` loop.
+                                                // "a" ka factorial chahiye
+// var i=1;
+// const a=7;        
+// let fact=1;
+// while(i<=a){
+//     fact = fact*i;
+//     i++
+// }
+// console.log("Factorial of",a,"is",fact);
+
+///////////////////////////////////////////////////////////////////////////
+// //7. Write a program using `if...else` to check if a student is pass or fail (pass if marks ≥ 40)
+// let marks = 67;
+// if(marks>=40){
+//     console.log("Student is pass");
+// }else{
+//     console.log("Student is fail");
+// }
+
+////////////////////////////////////////////////////////////////////
+// 8. Print the sum of first 5 odd numbers using a `while` loop.
+
+// count = 0;
+// let i=0;
+// while(count<=5){
+//     if(i%2!==0){
+//         count= count+i;
+//         i++;
+//     }else{
+//         i++;
+//     }
+//     console.log(count);
+    
+// }
+
+/////////////////////////////////////////////////////////////////////
+//9. Check if a number is divisible by both 4 and 6 using a single condition.
+
+// i = 8;
+// if(i%4==0 && i%6==0){
+//     console.log(i,"is divisible by both 4 and 6");
+// }else{
+//     console.log(i,"is not divisible by both 4 and 6");
+// }
+
+
+// //if(i%4==0){
+//     console.log(i,"is divisible by 4");
+// }if(i%6==0) {
+//     console.log(i,"is divisible by 4");
+
+/////////////////////////////////////////////////////////////////////
+//10. Reverse a number using a `while` loop (e.g., 4321 → 1234).
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////
+// //12. Write a program to check whether a number is a multiple of 5 or not
+// i= 23;
+// if(i%5==0){
+//     console.log(
+//         i,"is divisible by 5"
+//     );
+// }else{
+//      console.log (i,"is not divisible by 5")
+// };
+
+//////////////////////////////////////////////////////////////////
+// // 15. Write a program to check which number is greater out of `a` and `b`
+// const a = 12;
+// const b = 12;
+
+// if(a>b){
+//     console.log(a,"is greater than",b);
+// }else if(a<b){
+//     console.log(b,"is greater than",a);
+// }else{
+//     console.log("both",a,"and",b,"are equal"); 
+// }
+
+////////////////////////////////////////////////////////////////
+// 17. Print all numbers between 1 and 30 that are divisible by 3 using a `while` loop
+// var i = 1;
+// while(i<=30){
+//     if(i%3==0)
+//         {console.log(i);}   
+//     i++;
+// }
+
+///////////////////////////////////////////////////////////////////
+// 18. Use `if...else if` to check:
+// - If number is positive → print "Positive"
+// - If 0 → print "Zero"
+// - Else → print "Negative"
+
+// const i = 0;
+
+// if(i>0){
+//     console.log("Positive");
+// }else if(i<0){
+//     console.log("Negative");
+// }else{
+//     console.log("Zero");
+// }
+
+////////////////////////////////////////////////////////////
+//19. Find the smallest number between 1 to 50 divisible by both 7 and 3
+
+// for(var i = 1;i<=50;i++){
+//     if(i%3==0 && i%7==0){
+//         console.log(i,"is divisible by 3 and 7");    
+//         break;
+//     }
+// }
+
+/////////////////////////////////////////////////////////////
+//22. Use a `while` loop to print the table of 6.
+
+// i=6;
+// var j = 1;
+// while(j<=10){
+//     console.log(i,"X",j,"=",i*j)
+//     j++;
+// }
+
+/////////////////////////////////////////////////////////////
+// //24. Print all even numbers between 1 and 20 using a `while` loop
+// var i = 1;
+// while(i<=20){
+//     if(i%2==0){
+//     console.log(i);
+//     }   
+//     i++;
+// }
+
+////////////////////////////////////////////////////////
+// //26. Write a program to check whether a year is a leap year or not
+//  let y = 2050;
+
+//  if(y%4==0){
+//     console.log(y,"is a leap year"); 
+//  }else{
+//     console.log(y,"is not a leap year");
+//  }
+
+//////////////////////////////////////////////////////////
+// 28. Write a program that prints "Fizz" if number divisible by 3, "Buzz" if divisible by 5,
+// //"FizzBuzz" if both.
+// let i= 105;
+// if(i%3==0 && i%5==0){
+//     console.log("FizzBuzz");
+// }else if(i%3==0){
+//     console.log("Fizz");
+// }else if(i%5==0){
+//     console.log("Buzz");
+// }else{
+//      console.log("Nuzz");
+// }
+
+///////////////////////////////////////////////////////////
+// 30. Count how many numbers between 1 to 100 are divisible by 9 using a `while` loop.
+
+
+
+
+
+
+
+
