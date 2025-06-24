a87ec4ff413102d98aa15caeef3386597629da74
Pk17007 <niparthkhillare@gmail.com>
2025-06-24 14:59:17 +0530

    c-10

--- java_basic/1.js
+++
@@ -1,5 +0,0 @@
-let i = 1;
-while(i<=20 && i%2==0){
-    console.log(i);
-    i++;
-}
---
+++ java_basic/ans2.js
@@ -0,0 +1,66 @@
+// file:///D:/NEW%20PARTH/SkillonIT_FED/js-assginments/JS%20loop%20and%20nested%20que.pdf
+//                      Q.16
+// count = "";
+// for(i=1;i<=4;i++){
+//     for(j=0;j<i;j++)
+//     {
+//         count = count + "*"
+//     }
+//     console.log(count)
+//     count = "";
+// }
+// *
+// **
+// ***
+// ****
+
+//////////////////////////////////////////////////
+// Q.17 
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
+// Q.18   Print the multiplication tables from 1 to 5 using nested loops
+
+// for(i=1;i<=5;i++){
+//     console.log("   Table of",i)
+//     for(j=1;j<=10;j++){
+//         console.log(i,"X",j,"=",j*i)
+//     }
+// }
+
+///////////////////////////////////////////////////////////////////////////
+// // 19. Print a square pattern using stars (5 rows and 5 columns)
+// count = '';
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//        count = count+"*"
+//     }
+//     console.log(count);
+//     count = ''
+// }
+
+/////////////////////////////////////////////////////////////////////////
+// // Q.20
+// count = "";
+// for(i=5;i>=1;i--){
+//     for(j=5;j>=1;j--){
+//         count = count + j;
+//     }
+//     console.log(count);
+//     count = '';
+// }
+
+//////////////////////////////////////////////////////////////////
+//21. Print a triangle pattern using alphabets
+
+
