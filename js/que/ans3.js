3bf2147a335606952f4d81b68656eeb41ff1b16b
Pk17007 <niparthkhillare@gmail.com>
2025-06-25 12:25:55 +0530

    c-11

---
+++ js/que/ans3.js
@@ -0,0 +1,19 @@
+//  file:///D:/NEW%20PARTH/SkillonIT_FED/js-assginments/JS%20Conditional%20statement.pdf
+
+
+
+// //8. Write an `if` statement to check if a variable is `undefined`
+// var i ="";
+// if(i){
+//     console.log("it is defined");    
+// }else{
+//     console.log("it is undefined");
+// }
+
+// wrong
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+
