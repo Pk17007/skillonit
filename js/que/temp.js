fc193d3880c4f33f4e1c442209f9340453d67bc8
Pk17007 <niparthkhillare@gmail.com>
2025-06-26 16:04:20 +0530

    c-12

--- js/que/temp.js
+++ js/que/temp.js
@@ -3,9 +3,9 @@ let a=[1,33,2,14,87,6,34]
 for (let i=0;i<a.length-1;i++){
     for (let j=0;j<a.length-i-1;j++){
         if(a[j]>a[j+1]){
-            let swapap = a[j]
+            let swap = a[j]
             a[j]=a[j+1]
-            a[j+1] = swapap
+            a[j+1] = swap
         }
     }
 }console.log(a);
