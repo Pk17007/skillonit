32c6f845c9b3a1350b278017ea7c8110dcf2285a
Pk17007 <niparthkhillare@gmail.com>
2025-07-02 16:11:09 +0530

    c-14

---
+++ js/dom23/assign/todo/index.js
@@ -0,0 +1,11 @@
+let task = document.getElementById("task")
+let priority = document.getElementById("priority")
+let tbody = document.querySelector("tbody")
+let form = document.querySelector("form")
+
+form.addEventListener("submit",submit)
+function submit(){
+    event.preventDefault;
+    tbody.
+
+}
