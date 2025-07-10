0d38340a664e5bc02cb729cb0a52dac33c5b4a7b
Pk17007 <niparthkhillare@gmail.com>
2025-07-10 16:40:54 +0530

    c-1

--- js/ecom/script/cart.js
+++ js/ecom/script/cart.js
@@ -1,5 +1,5 @@
 cart.addEventListener("click",cartfun);
 
-function cartfun() {
-    window.location.href = "http://127.0.0.1:5500/js/ecom/html/cart.html"
-}
+// function cartfun() {
+//     window.location.href = "http://127.0.0.1:5500/js/ecom/html/cart.html"
+// }
