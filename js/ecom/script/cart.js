0f5274deffa45d7f468479a10a002e380e3bc803
Pk17007 <niparthkhillare@gmail.com>
2025-07-11 12:05:09 +0530

    c-1

--- js/ecom/script/cart.js
+++ js/ecom/script/cart.js
@@ -1,28 +1,58 @@
-let cartdata = JSON.parse(localStorage.getItem("cdata")) || []
-let main = document.getElementById("main")
+let c_data = JSON.parse(localStorage.getItem("cdata")) || []
+let main1 = document.getElementById("main")
 
 function grid(arr){
     arr.map( function(el){
         
         let box = document.createElement("div")
-        let des = document.createElement("div")
-        let p = document.createElement("p")
-        
+
+           let  quantity = 1 || el.quantity;
+        el.quantity = quantity ;
 
         let img = document.createElement("img")
         img.src = el.image
 
+        let price = document.createElement("h3")
+        price.innerText = `$  ${el.price * quantity}`
+
+        let Q = document.createElement("div")
+
+        let quan = document.createElement("p")
+        quan.innerText = quantity;
+
+        let plus = document.createElement("button")
+        plus.innerText = "+";
+        plus1();
+        
+        let minus = document.createElement("button")
+        minus.innerText = "-";
+        minus1();
+
         let title = document.createElement("h3")
         title.innerText = el.title
+        Q.append(plus,quan,minus)
 
-        p.innerText = el.description;
-        des.append(p)
-        box.append(img,title,des)
-        main.append(box)
+        box.append(img,title,price,Q)
+        
+        main1.append(box)
     })
 
 
 
 }
 
-grid(cartdata);
+grid(c_data);
+
+
+function plus1(){
+    addEventListener("click", function(){
+        quantity++;
+        
+    })
+}
+function minus1(){
+    addEventListener("click", function(){
+        quantity--;
+        
+    })
+}
