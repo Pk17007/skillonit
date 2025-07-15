bb5a170c41d09f7f4521b1197ede041b42b1230e
Pk17007 <niparthkhillare@gmail.com>
2025-07-15 14:06:43 +0530

    c-3

---
+++ js_adv/constructor/index.js
@@ -0,0 +1,27 @@
+// let obj = {}
+// obj.name = "name2"
+// obj.num = 12
+
+// console.log(obj)
+
+
+
+// function Object(){
+//     this.name = "name4"
+//     this.num = 76
+// }
+// console.log(Object) // ƒ Object(){
+                        //  this.name = "name4"
+                        //   this.num = 76
+                        //     }
+
+
+
+// Object()  // Won't show any output in console
+
+// x = Object();
+// console.log(x);   // undefined in console
+
+
+
+
