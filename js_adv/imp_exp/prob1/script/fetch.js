4b0b5f785e9fc601a363935c76718039c45b292f
Pk17007 <niparthkhillare@gmail.com>
2025-08-03 20:41:57 +0530

    ce

---
+++ js_adv/imp_exp/prob1/script/fetch.js
@@ -0,0 +1,27 @@
+let api = "https://fakestoreapi.com/products"
+// const api2 = "https://jsonplaceholder.typicode.com/photos";
+
+
+async function fetch(){
+    try {
+        let res = await fetch(api) 
+    let x = await res.json()
+       console.log(x);
+
+    data_map(x)
+        
+    } catch (error) {
+        console.log("err")
+      
+    }
+}
+
+fetch()
+
+
+const data_map=(data)=>{
+    data.map((el)=>{
+        let p = document.createElement("p")
+        p.innerText = el.title
+    })
+}
